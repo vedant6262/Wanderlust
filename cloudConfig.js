a4532c55183472202d3326b868f8a8230bda1478
Vedant Pisal <studentat@sigma8.0 batch>
2026-02-01 02:54:14 +0530

    Fix Cloudinary env vars and allowed_formats for production

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,16 +2,16 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.ClOUD_NAME,
-  api_key: process.env.ClOUD_API_KEY,
-  api_secret: process.env.ClOUD_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "wanderlust_DEV",
-    allowedFormats: ["png", "jpg", "jpeg"],
+    allowed_formats: ["jpg", "jpeg", "png"],
   },
 });
 
